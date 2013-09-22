bd99d0350c0deb9fdb36ed66cbbab78037277841
MrProg224 <tankrobert453@yahoo.com>
2013-09-22 01:29:48 -0400

    Add NetworkMod

--- Common/Common.java
+++ Common/Common.java
@@ -1,9 +1,11 @@
 package Mod.enchantedwoodlands.Common;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
 
 
-@Mod(modid = Common.modid, name = "Enchanted Woodlands Mod", version = "0.01")
+@Mod(modid = Common.modid, name = "Enchanted Woodlands Mod", version = "0.0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class Common {
 	
 	public static final String modid = "Enchantedwoodlands";
