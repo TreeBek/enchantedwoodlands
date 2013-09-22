a8fb5a29cc36103a86605a696c7d42b6034e37bd
TreeBek <treebekgaming@gmail.com>
2013-09-22 00:35:26 -0500

    Config

---
+++ Config/Config.java
@@ -0,0 +1,14 @@
+package Mod.enchantedwoodlands.Config;
+
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Config {
+	
+	@EventHandler
+	public static void ConfigInit(FMLPreInitializationEvent event){
+		
+		
+	}
+	
+}
