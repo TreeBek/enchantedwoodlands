d63ca9732f2c7cb658c9f59fadbbb48a75c3e599
MrProg224 <tankrobert453@yahoo.com>
2013-09-22 01:39:43 -0400

    TestBlock

--- Config/Config.java
+++ Config/Config.java
@@ -1,13 +1,20 @@
 package Mod.enchantedwoodlands.Config;
 
+import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class Config {
 	
+	public static int testBlockID;
+	
 	@EventHandler
 	public static void ConfigInit(FMLPreInitializationEvent event){
 		
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		
+		testBlockID = config.get(Configuration.CATEGORY_BLOCK, "TEST BLOCK", 2500).getInt();
 		
 	}
 	
