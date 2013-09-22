1335fe50a781fde2b09bde31dec4edf609d3b8f0
MrProg224 <tankrobert453@yahoo.com>
2013-09-22 01:19:30 -0400

    Update Common.java

--- Common/Common.java
+++ Common/Common.java
@@ -3,7 +3,7 @@ package Mod.enchantedwoodlands.Common;
 import cpw.mods.fml.common.Mod;
 
 
-@Mod(modid = Common.modid, name = "Enchanted Woodlands Mod", version = "0.01")
+@Mod(modid = Common.modid, name = "Enchanted Woodlands Mod", version = "0.0.1")
 public class Common {
 	
 	public static final String modid = "Enchantedwoodlands";
