924eb83d9849e89e0b7b4363d68dd01911a6a3d4
TreeBek <treebekgaming@gmail.com>
2013-09-22 00:18:45 -0500

    first commit

---
+++ Common/Common.java
@@ -0,0 +1,11 @@
+package Mod.enchantedwoodlands.Common;
+
+import cpw.mods.fml.common.Mod;
+
+
+@Mod(modid = Common.modid, name = "Enchanted Woodlands Mod", version = "0.01")
+public class Common {
+	
+	public static final String modid = "Enchantedwoodlands";
+
+}
