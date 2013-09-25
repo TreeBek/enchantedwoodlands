e2247a23ad23ff86e1bfd1b1ff5535c9f6675c46
MrProg224 <tankrobert453@yahoo.com>
2013-09-24 21:43:21 -0400

    Fixed Flower Generation

--- Blocks/BlockStarDustFlower.java
+++ Blocks/BlockStarDustFlower.java
@@ -2,12 +2,26 @@ package Mod.enchantedwoodlands.Blocks;
 
 import static net.minecraftforge.common.EnumPlantType.Plains;
 
+import java.util.List;
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import Mod.enchantedwoodlands.Common.Common;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFlower;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 
@@ -19,30 +33,12 @@ public class BlockStarDustFlower extends BlockFlower
 	{
 		super(id);
 		this.setCreativeTab(Common.EnchWoodRPGTab);
-		this.setStepSound(soundGrassFootstep);
 	}
-	
-	public void upadteTick(World world, int i, int j, int k, Random random)
-	{
-		if(random.nextInt(100)==0)
-		{
-			int l = (i + random.nextInt(3)) - 1; 
-			int il = (j + random.nextInt(2)) - random.nextInt(2);
-			int jl = (k + random.nextInt(3)) - 1;
-			if(world.isAirBlock(l, il, jl) && canBlockStay(world, l, il, jl))
-				{
-					world.notifyBlockChange(l, il, jl, blockID);
-				}
-			}
-		}
-		
-		
-	
 	
 	@Override
     protected boolean canThisPlantGrowOnThisBlockID(int par1)
     {
-		return par1 == Block.grass.blockID || par1 == Block.dirt.blockID || par1 == Block.tilledField.blockID || par1 == Blocks.starDustGrass.blockID; 
+        return par1 == Blocks.starDustGrass.blockID || par1 == Blocks.woodlandGrass.blockID;
     }
 	
     @Override
@@ -55,19 +51,6 @@ public class BlockStarDustFlower extends BlockFlower
     @Override
     public void registerIcons(IconRegister reg)
     {
-    	this.blockIcon = reg.registerIcon("MythicalRPG:starDustFlower");
-    }
-    
-    @Override
-    public boolean canPlaceBlockAt(World world, int x, int y, int z)
-    {	
-    	if(world.getBlockId(x, y+1, z) == Blocks.starDustGrass.blockID)
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return false;
-    	}
+    	this.blockIcon = reg.registerIcon("EnchantedWoodlands:starDustFlower");
     }
 }
--- Dimension/Plants/WorldGenMythicFlowers.java
+++ Dimension/Plants/WorldGenMythicFlowers.java
@@ -4,92 +4,39 @@ import java.util.Random;
 
 import Mod.enchantedwoodlands.Blocks.Blocks;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenMythicFlowers extends WorldGenerator
 {
-    /** The block ID of the ore to be placed using this generator. */
-    private int minableBlockId;
-    private int minableBlockMeta = 0;
+	/** The ID of the plant block used in this plant generator. */
+	private int plantBlockId = Blocks.starDustFlower.blockID;
+	private int plantBlockMeta;
 
-    /** The number of blocks to generate. */
-    private int numberOfBlocks;
-    private int field_94523_c;
+	public WorldGenMythicFlowers(int par1, int meta)
+	{
+		plantBlockId = par1;
+		plantBlockMeta = meta;
+	}
 
-    public WorldGenMythicFlowers(int par1, int par2)
-    {
-        this(par1, par2, Blocks.starDustGrass.blockID);
-    }
+	@Override
+	public boolean generate(World par1World, Random par2Random, int par3, int par4, int par5)
+	{
+		for (int l = 0; l < 64; ++l)
+		{
+			int i1 = par3 + par2Random.nextInt(8) - par2Random.nextInt(8);
+			int j1 = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
+			int k1 = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
 
-    public WorldGenMythicFlowers(int par1, int par2, int par3)
-    {
-        this.minableBlockId = par1;
-        this.numberOfBlocks = par2;
-        this.field_94523_c = par3;
-    }
+			if (par1World.isAirBlock(i1, j1, k1) && (!par1World.provider.hasNoSky || j1 < 127) && (par1World.getFullBlockLightValue(i1, j1, k1) >= 8 || par1World.canBlockSeeTheSky(i1, j1, k1))
+					&& Block.blocksList[plantBlockId].canPlaceBlockOnSide(par1World, i1, j1, k1, 1, new ItemStack(plantBlockId, 1, plantBlockMeta)))
+			{
+				par1World.setBlock(i1, j1, k1, plantBlockId, plantBlockMeta, 2);
+			}
+		}
 
-    public WorldGenMythicFlowers(int id, int meta, int number, int target)
-    {
-        this(id, number, target);
-        minableBlockMeta = meta;
-    }
-
-    public boolean generate(World par1World, Random par2Random, int par3, int par4, int par5)
-    {
-        float f = par2Random.nextFloat() * (float)Math.PI;
-        double d0 = (double)((float)(par3 + 8) + MathHelper.sin(f) * (float)this.numberOfBlocks / 8.0F);
-        double d1 = (double)((float)(par3 + 8) - MathHelper.sin(f) * (float)this.numberOfBlocks / 8.0F);
-        double d2 = (double)((float)(par5 + 8) + MathHelper.cos(f) * (float)this.numberOfBlocks / 8.0F);
-        double d3 = (double)((float)(par5 + 8) - MathHelper.cos(f) * (float)this.numberOfBlocks / 8.0F);
-        double d4 = (double)(par4 + par2Random.nextInt(3) - 2);
-        double d5 = (double)(par4 + par2Random.nextInt(3) - 2);
-
-        for (int l = 0; l <= this.numberOfBlocks; ++l)
-        {
-            double d6 = d0 + (d1 - d0) * (double)l / (double)this.numberOfBlocks;
-            double d7 = d4 + (d5 - d4) * (double)l / (double)this.numberOfBlocks;
-            double d8 = d2 + (d3 - d2) * (double)l / (double)this.numberOfBlocks;
-            double d9 = par2Random.nextDouble() * (double)this.numberOfBlocks / 16.0D;
-            double d10 = (double)(MathHelper.sin((float)l * (float)Math.PI / (float)this.numberOfBlocks) + 1.0F) * d9 + 1.0D;
-            double d11 = (double)(MathHelper.sin((float)l * (float)Math.PI / (float)this.numberOfBlocks) + 1.0F) * d9 + 1.0D;
-            int i1 = MathHelper.floor_double(d6 - d10 / 2.0D);
-            int j1 = MathHelper.floor_double(d7 - d11 / 2.0D);
-            int k1 = MathHelper.floor_double(d8 - d10 / 2.0D);
-            int l1 = MathHelper.floor_double(d6 + d10 / 2.0D);
-            int i2 = MathHelper.floor_double(d7 + d11 / 2.0D);
-            int j2 = MathHelper.floor_double(d8 + d10 / 2.0D);
-
-            for (int k2 = i1; k2 <= l1; ++k2)
-            {
-                double d12 = ((double)k2 + 0.5D - d6) / (d10 / 2.0D);
-
-                if (d12 * d12 < 1.0D)
-                {
-                    for (int l2 = j1; l2 <= i2; ++l2)
-                    {
-                        double d13 = ((double)l2 + 0.5D - d7) / (d11 / 2.0D);
-
-                        if (d12 * d12 + d13 * d13 < 1.0D)
-                        {
-                            for (int i3 = k1; i3 <= j2; ++i3)
-                            {
-                                double d14 = ((double)i3 + 0.5D - d8) / (d10 / 2.0D);
-                                Block block = Block.blocksList[par1World.getBlockId(k2, l2, i3)];
-
-                                if (d12 * d12 + d13 * d13 + d14 * d14 < 1.0D && (block != null && block.isGenMineableReplaceable(par1World, k2, l2, i3, field_94523_c)))
-                                {
-                                    par1World.setBlock(k2, l2, i3, this.minableBlockId, minableBlockMeta, 2);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
+		return true;
+	}
 }
-
